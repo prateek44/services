6ed699ab27c34238fb9b46ed74e45b9c518892e3
prateek44 <44beastmodeon@gmail.com>
2018-05-22 11:26:33 +0530

    custom service application to add items to shoppingList

--- app.js
+++ app.js
@@ -2,10 +2,51 @@
 {
   'use strict';
   angular.module("serviceApp",[])
-  .controller("serviceAppController",['$scope',serviceAppController])
-  .service("",);
-  function serviceAppController($scope)
+  .controller("addToListController",['addingItemsToListService',addToListController])
+  .controller("showListController",['addingItemsToListService',showListController])
+  .service("addingItemsToListService",addingItemsToListService);
+  function addToListController(addingItemsToListService)
   {
+    console.log('addToListController');
 
+    var addList=this;
+    addList.itemName="";
+    addList.itemQty="";
+    addList.addToItem=function()
+    {
+      console.log("invoking addingItemsToListService ");
+      addingItemsToListService.addToItems(addList.itemName,addList.itemQty);
+    };
+
+  }
+  function showListController(addingItemsToListService)
+  {
+    console.log('showListController');
+    var showList=this;
+    showList.addedItemList=addingItemsToListService.getItems();
+    // showList.getItemList=function()
+    // {
+    //   console.log('invoking service to fetch the added item list');
+    //   addingItemsToListService.getItems();
+    // };
+  }
+  function addingItemsToListService()
+  {
+    var service=this;
+      var itemList=[];
+    console.log('addingItemsToListService...');
+    service.addToItems=function(currentItemName,currentItemQty)
+    {
+      console.log("adding items to the list");
+
+      var currentItem={name:currentItemName,quantity:currentItemQty};
+      itemList.push(currentItem);
+      console.log("item added successfully");
+    };
+    service.getItems=function()
+    {
+      console.log('service invoked to fetch the added item list');
+      return itemList;
+    };
   }
 })();
